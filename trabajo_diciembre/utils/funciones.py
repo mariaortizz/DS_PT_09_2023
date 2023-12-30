7a7e889cf0f58fd864d35f8fb66c009930de0128
Maria del Rosario Ortiz <145385918+mariaortizz@users.noreply.github.com>
2023-12-30 16:21:51 +0100

    funciones update, new folder

---
+++ trabajo_diciembre/utils/funciones.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from scipy.stats import kurtosis, skew, probplot, shapiro, spearmanr, kruskal
+from geopy.geocoders import Nominatim
+import time
+import pandas as pd
+import plotly.express as px
+pd.set_option('display.max_columns', None)
+
+def cardinalidad(data):
+    '''Funcion para saber la cardinalidad de las varibales que tenemos en el data frame'''
+    df_cardin = pd.DataFrame([{
+                'variable' : i,
+                'tipo_dato' : data[i].dtypes,
+                'cantidad_de_nulos' : data[i].isna().sum(),
+                'valores_unicos' : data[i].unique(),
+                'cardinalidad' : data[i].nunique(),
+                'porcentaje_cardinalidad' : (data[i].nunique()/data.shape[0])*100
+            } for i in data])
+    
+    df_tipo_variable = pd.DataFrame({'tipo_variable' : ['discreta', 'continua', 'continua', 'ordinal',
+            'ordinal', 'nominal', 'nominal', 'nominal', 'nominal',
+            'nominal', 'nominal', 'nominal', 'nominal', 'nominal',
+            'nominal', 'nominal', 'nominal', 'nominal', 'discreta',
+            'discreta', 'continua', 'nominal']}) #TODO
+    
+    df_cardinalidad = pd.concat([df_cardin,df_tipo_variable], axis = 1)
+
+    return df_cardinalidad
+
+def graficos_variables_cuant(data):
+    ''''Función para graficar las variables cuantitativas'''
+    media_color = 'r'
+    mediana_color = 'b'
+    try:
+        for columna in data.columns:
+            print('--'*30)
+            print(f"VARIABLE: {columna}\n")
+
+            media = data[columna].mean()
+            mediana = data[columna].median()
+
+            plt.figure(figsize=(20,4))
+            sns.boxplot(data[columna], orient='h')
+            plt.axvline(media, color = media_color, linestyle = 'dashed', linewidth = 1)
+            plt.axvline(mediana, color = mediana_color, linestyle = 'dashed', linewidth = 1)
+
+            plt.show()
+
+            sns.displot(data[columna], rug = True, bins = 30)
+            plt.axvline(media, color = media_color, linestyle = 'dashed', linewidth = 1, label = f'Media: {media:.0f}')
+            plt.axvline(mediana, color = mediana_color, linestyle = 'dashed', linewidth = 1, label = f'Mediana: {mediana:.0f}')
+
+            plt.title(f'Distribución de {columna}')
+            plt.legend()
+
+            plt.show()
+
+            print(data[columna].describe().round())
+            print('--'*30)
+    except Exception as a:
+        print(f"No puedo analizar la variable por este error {a}")
+
+def graficos_variables_cualit(data):
+    ''''Función para graficar las variables cualitativas'''
+    try:
+        for columna in data.columns:
+            print('--'*50)
+            print(f"VARIABLE: {columna}\n")
+            if len(data[columna].dropna().unique()) > 1:
+                ax = sns.countplot(data= data.sort_values(by=columna), x= columna, palette='husl')
+                ax.set_xticklabels(data[columna].sort_values().unique(), rotation=90)
+                #se superponen los valores porque me da uns advertencia al aplicar este parametro, no sé como sacarla :)
+                plt.title(f"Conteo variable {columna}")
+                plt.show();
+            else:
+                print('No es necesario graficar porque tiene un solo valor dentro de la columna')
+            print(data[columna].value_counts())
+            print('--'*50)
+    except Exception as a:
+        print(f"No puedo analizar la variable por este error {a}")
+
+def graficos_box_plot(data):
+    ''''Función para graficar las variables mediante grafico tipo Boxplot'''
+    media_color = 'r'
+    mediana_color = 'b'
+    try:
+        for columna in data.columns:
+            print('--'*30)
+            print(f"VARIABLE: {columna}\n")
+
+            media = data[columna].mean()
+            mediana = data[columna].median()
+
+            plt.figure(figsize=(20,4))
+            sns.boxplot(data[columna], orient='h')
+            plt.axvline(media, color = media_color, linestyle = 'dashed', linewidth = 1)
+            plt.axvline(mediana, color = mediana_color, linestyle = 'dashed', linewidth = 1)
+
+            plt.show()
+
+            print(data[columna].describe().round())
+            print('--'*30)
+    except Exception as a:
+        print(f"No puedo analizar la variable por este error {a}")
